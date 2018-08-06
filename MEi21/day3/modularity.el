3a2f0f86cb3346350ccdc663bf74a5c65f5593f5
joseph-zhang <zyt5002@126.com>
2018-08-06 21:51:40 +0800

    add modularity to day3

---
+++ MEi21/day3/modularity.el
@@ -0,0 +1,7 @@
+;; in previous code, all the configuration are written in one file
+;; we know that configuration is always complex,
+;; use modularity can make the configuration clear.
+
+;; for example, in a common configuration ~/.emacs.d/init.el file
+;; there are many different code used for different way, some for UI, some for keybinding ..
+;; it is better to write then separately in different .el files.
