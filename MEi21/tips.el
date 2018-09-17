9c0fb1dc7a08f418e4fab7f112c222cb4a798ac0
joseph-zhang <zyt5002@126.com>
2018-09-17 16:27:30 +0800

    modeified tips

--- MEi21/tips.el
+++ MEi21/tips.el
@@ -2,8 +2,8 @@
 
 ;; 1) to enter SPC in emacs-mode, you should use M-m first, not white space;
 ;; 2) if the installation of spacemacs meet an 'mispell' error, try emacs with -inscure;
-;; 3) if some package are not installed correctly, just delete them in elpa, and rebbot the emacs;
-;; 4) to change the modeline separator effect, use
+;; 3) if some package are not installed correctly, just delete them in elpa, and reboot the emacs;
+;; 4) to change the modeline separator effect, add the following line in dotspacemacs/user-config:
 (setq powerline-default-separator xxx)
 ;; 5) some inner layers can be added in dotspacenacs/layers -> dotspacemacs-configuration-layers;
 ;; 6) the default setting of spacemacs is no line number, to take on it, find
