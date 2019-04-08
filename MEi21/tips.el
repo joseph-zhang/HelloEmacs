16610844b3305ffabfca76299fe7be4d678c17d2
Metaphia <zyt5002@126.com>
2019-04-08 20:29:29 +0800

    Update tips.el

--- MEi21/tips.el
+++ MEi21/tips.el
@@ -1,7 +1,7 @@
 ;;;; here are some tips when using emacs/spacemacs the first time.
 
 ;; 1) to enter SPC in emacs-mode, you should use M-m first, not white space;
-;; 2) if the installation of spacemacs meet an 'mispell' error, try emacs with -inscure;
+;; 2) if the installation of spacemacs meet an 'mispell' error, try emacs with --insecure;
 ;; 3) if some package are not installed correctly, just delete them in elpa, and reboot the emacs;
 ;; 4) to change the modeline separator effect, add the following line in dotspacemacs/user-config:
 (setq powerline-default-separator xxx)
